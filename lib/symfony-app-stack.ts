569d7be268bfd3cb33aef75a974dc5c47c25bef8
abdoutony <abouaallb@gmail.com>
2023-01-09 11:14:40 +0100

    i

--- lib/symfony-app-stack.ts
+++ lib/symfony-app-stack.ts
@@ -2,8 +2,15 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+interface SymfonyAppProps extends cdk.StackProps {
+  readonly dev: boolean;
+}
+
+
+
 export class SymfonyAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  //scope: Construct, id: string, props?: cdk.StackProps
+  constructor(scope: Construct, id: string, props: SymfonyAppProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
